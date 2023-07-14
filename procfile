f416368d08a169f0c38878469b3c64b7ddde8bb9
Jawad Abbas Khan <fa17-bse-078@cuiwah.edu.pk>
2023-07-15 00:47:22 +0500

    v 2.0

---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn videoconferencing.wsgi --log-file -
--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6fdgh7tki&^y_-cl4%tpyl-$kh9($vos&a5q&uq%!az2gs__bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
